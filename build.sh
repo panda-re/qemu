2d621b27ddbed3df9dd0e781c69ecd2e6e847edb
Luke Craig <lacraig3@gmail.com>
2022-10-12 10:56:29 -0400

    build script for convenience

---
+++ build.sh
@@ -0,0 +1,5 @@
+PANDA_DIR_REL="$(dirname $0)"
+TARGETS="x86_64-softmmu,i386-softmmu,arm-softmmu,aarch64-softmmu,ppc-softmmu,ppc64-softmmu,mips-softmmu,mipsel-softmmu,mips64-softmmu,mips64el-softmmu"
+
+"${PANDA_DIR_REL}/configure" --enable-plugins --target-list=$TARGETS
+ninja
