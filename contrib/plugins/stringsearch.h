2c1785ac4a7917f7f8fe07ec9ad1a1cf71f43a6b
jamcleod <Jordan.McLeod@ll.mit.edu>
2023-04-12 15:57:54 -0400

    Add initial skeleton for pngsearch plugin

--- contrib/plugins/stringsearch.h
+++ contrib/plugins/stringsearch.h
@@ -3,5 +3,6 @@
 
 QPP_FUN_PROTOTYPE(stringsearch, bool, stringsearch_remove_string, const char* arg_str);
 QPP_FUN_PROTOTYPE(stringsearch, void, stringsearch_reset_strings, void);
+QPP_FUN_PROTOTYPE(stringsearch, bool, stringsearch_add_string, const char* arg_str);
 
 #endif /* STRINGSEARCH_H */
