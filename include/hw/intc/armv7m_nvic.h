f7cd4077143625113621fcbb51eaf07efecb3655
Florian Albrecht <florian.albrecht.public@outlook.de>
2023-06-08 17:42:36 +0200

    WIP: Fixing interrupt injection

--- include/hw/intc/armv7m_nvic.h
+++ include/hw/intc/armv7m_nvic.h
@@ -192,4 +192,11 @@ int armv7m_nvic_raw_execution_priority(NVICState *s);
 bool armv7m_nvic_neg_prio_requested(NVICState *s, bool secure);
 bool armv7m_nvic_can_take_pending_exception(NVICState *s);
 
+/* Avatar2 Addition to enable interrupt injection
+ */
+#ifdef CONFIG_AVATAR
+bool armv7m_nvic_enable_all_irqs(void *opaque);
+#endif
+
+
 #endif
