1283499fd5da7fffbddb2035df3cf950af934ded
aking <aking@mit.edu>
2025-01-07 15:25:26 -0500

    Create and update more C++ hackery for PANDA cheaders.h

--- include/panda/cheaders.h
+++ include/panda/cheaders.h
@@ -6,13 +6,26 @@
 #ifdef __cplusplus
 #include <type_traits>
 #pragma push_macro("new")
-#pragma push_macro("typename")
 #pragma push_macro("typeof")
 #pragma push_macro("export")
 #define new pandanew
-#define typename
 #define typeof(x) std::remove_const<std::remove_reference<decltype(x)>::type>::type
 #define export pandaexport
+
+#pragma push_macro("typename")
+#define typename typename__
+
+#pragma push_macro("typeof_strip_qual")
+#define typeof_strip_qual(expr) std::remove_cv<typeof(expr)>::type
+
+#pragma push_macro("class")
+#define class _csymbol_named_class
+
+#pragma push_macro("_Static_assert")
+#define _Static_assert static_assert
+
+#pragma push_macro("__builtin_types_compatible_p")
+#define __builtin_types_compatible_p(T1, T2) (std::is_same<std::remove_cv<T1>::type, std::remove_cv<T2>::type>::value ? 1 : 0)
 
 extern "C" {
 #endif
@@ -36,6 +49,10 @@ extern "C" {
 #pragma pop_macro("typename")
 #pragma pop_macro("typeof")
 #pragma pop_macro("export")
+#pragma pop_macro("typeof_strip_qual")
+#pragma pop_macro("class")
+#pragma pop_macro("_Static_assert")
+#pragma pop_macro("__builtin_types_compatible_p")
 #endif
 
 #endif
