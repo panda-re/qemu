7e5c55ca4e4f8ca71fb8d0dfea17f56c10141ff6
Luke Craig <lacraig3@gmail.com>
2025-01-22 13:01:31 -0500

    fixup panda_get_tb

--- include/panda/panda_qemu_plugin_helpers.h
+++ include/panda/panda_qemu_plugin_helpers.h
@@ -1,4 +1,4 @@
 CPUState *panda_current_cpu(int index);
 CPUState *panda_cpu_in_translate(void);
-TranslationBlock *panda_get_tb(void);
+TranslationBlock *panda_get_tb(struct qemu_plugin_tb *tb);
 int panda_get_memcb_status(void);
