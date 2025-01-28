70365762e7c785eacb292366d866d36f9db729e6
Luke Craig <lacraig3@gmail.com>
2025-01-27 22:39:44 -0500

    mis: add panda_guest_hypercall

--- target/mips/tcg/translate.c
+++ target/mips/tcg/translate.c
@@ -35,6 +35,7 @@
 #include "exec/helper-info.c.inc"
 #undef  HELPER_H
 
+#include "panda/callbacks/cb-helper-impl.h"
 
 /*
  * Many system-only helpers are not reachable for user-only.
@@ -2702,6 +2703,7 @@ static void gen_cond_move(DisasContext *ctx, uint32_t opc,
 
     if (rd == 0) {
         /* If no destination, treat it as a NOP. */
+        gen_helper_panda_guest_hypercall();
         return;
     }
 
@@ -8495,6 +8497,7 @@ static void gen_cp0(CPUMIPSState *env, DisasContext *ctx, uint32_t opc,
     case OPC_MFC0:
         if (rt == 0) {
             /* Treat as NOP. */
+            gen_helper_panda_guest_hypercall();
             return;
         }
         gen_mfc0(ctx, cpu_gpr[rt], rd, ctx->opcode & 0x7);
